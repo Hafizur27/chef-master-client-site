71b5f4e2ad734da9fdee013a5f4c0f5912a3ce6f
hafizur27 <mahmadhafiz33@gmail.com>
2023-05-03 12:56:16 +0600

    nav-bar done

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,12 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { FaUserTie } from "react-icons/fa";
 
 const Header = () => {
-    return (
-        <div>
-            <p>my header page here</p>
-        </div>
-    );
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="success" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Chef-Master</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav className="text-dark">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#blog">Blog</Nav.Link>
+            <Nav.Link
+              eventKey={2}
+              href="#login"
+              className="d-flex align-items-center"
+            >
+              Login
+              <FaUserTie />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
